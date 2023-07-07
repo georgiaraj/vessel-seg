91b5a54d7cbb120d5b496bd84f253df8bfa490e1
Georgia Rajamanoharan <g.l.rajamanoharan@gmail.com>
2023-07-07 21:01:23 +0100

    train: Add the initial training loop. Not quite working yet.

--- train.py
+++ train.py
@@ -1,27 +1,88 @@
 import argparse
 
+import torch
+from torch.utils.data import DataLoader
+
 from model import UNet
 from datasets import data
 
 
 def get_args():
     parser = argparse.ArgumentParser('Vessel Segmentation')
-    parser.add_argument('data_dir', type=str, help='Directory for the data')
+    parser.add_argument('train_data_dir', type=str, help='Directory for the train data')
+    parser.add_argument('test_data_dir', type=str, help='Directory for the test data')
     parser.add_argument('--train-videos', default=None, nargs='+', type=str,
                         help='Videos to use for train. If empty all are used.')
     parser.add_argument('--test-videos', default=None, nargs='+', type=str,
                         help='Videos to use for test. If empty all are used.')
+    parser.add_argument('--batch-size', default=32, type=int)
     return parser.parse_args()
-    
-    
+
+def train(model, train_dataloader, val_dataloader, learning_rate=0.001, num_epochs=20):
+
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    def train_one_epoch(epoch_index):
+        running_loss = 0.
+        last_loss = 0.
+
+        for i, data in enumerate(train_dataloader):
+            inputs, labels = data
+
+            optimizer.zero_grad()
+
+            outputs = model(inputs)
+
+            loss = loss_fn(outputs, labels)
+            loss.backward()
+
+            optimizer.step()
+
+            running_loss += loss.item()
+
+            if i % 100 == 99:
+                last_loss = running_loss / 100
+                print(f'batch {i} loss: {last_loss}')
+                running_loss = 0.
+
+        return last_loss
+
+    for epoch in range(num_epochs):
+
+        model.train(True)
+        avg_loss = train_one_epoch(epoch)
+
+        running_vloss = 0.0
+
+        model.eval()
+
+        with torch.no_grad():
+            for i, vdata in enumerate(val_dataloader):
+                vinputs, vlabels = vdata
+                voutputs = model(vinputs)
+                vloss = loss_fn(voutputs, vlabels)
+                running_vloss += vloss
+
+        avg_vloss = running_vloss / (epoch + 1)
+        print(f'train loss: {avg_loss} val_loss: {avg_vloss}')
+
+
 if __name__ == '__main__':
 
     args = get_args()
-    
+
     model = UNet(3, 8)
 
     print(model)
 
-    dataset = data['vessel_data'](args.data_dir)
+    train_dataset = data['vessel_data'](args.train_data_dir)
+    test_dataset = data['vessel_data'](args.test_data_dir)
 
-    print(dataset)
+    print(train_dataset)
+    print(test_dataset)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False)
+
+    train(model, train_dataloader, val_dataloader)
