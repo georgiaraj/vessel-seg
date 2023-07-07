dc7e55fe33272de0362e235eb645432b18b1affa
Georgia Rajamanoharan <g.l.rajamanoharan@gmail.com>
2023-07-07 14:37:54 +0100

    train: Allow user to set root dir of data.

--- train.py
+++ train.py
@@ -1,8 +1,27 @@
+import argparse
+
 from model import UNet
 from datasets import data
 
-def train_unet():
+
+def get_args():
+    parser = argparse.ArgumentParser('Vessel Segmentation')
+    parser.add_argument('data_dir', type=str, help='Directory for the data')
+    parser.add_argument('--train-videos', default=None, nargs='+', type=str,
+                        help='Videos to use for train. If empty all are used.')
+    parser.add_argument('--test-videos', default=None, nargs='+', type=str,
+                        help='Videos to use for test. If empty all are used.')
+    return parser.parse_args()
+    
+    
+if __name__ == '__main__':
+
+    args = get_args()
+    
     model = UNet(3, 8)
 
     print(model)
 
+    dataset = data['vessel_data'](args.data_dir)
+
+    print(dataset)
