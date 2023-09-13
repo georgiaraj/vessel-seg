9282845c6a19bc1c17b3a8b2352e62c2ccfbc3a4
Georgia Rajamanoharan <g.l.rajamanoharan@gmail.com>
2023-09-13 20:49:59 +0100

    model: More changes, still not quite working...

--- model.py
+++ model.py
@@ -1,13 +1,16 @@
 import pdb
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 class DoubleConv(nn.Module):
 
-    def __init__(self, in_channels, out_channels, kernel_size=3):
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, bias=False)
+    def __init__(self, in_channels, out_channels, kernel=3):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, bias=False)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
@@ -17,7 +20,7 @@ class DoubleConv(nn.Module):
         x = self.conv2(x)
         x = self.bn2(x)
         x = torch.relu(x)
-
+        return x
 
 class UNet(nn.Module):
 
@@ -25,24 +28,26 @@ class UNet(nn.Module):
         super().__init__()
 
         num_channels = 3
-        num_filters = init_filters
+        num_filters = init_filters // 2
         encoders = []
         decoders = []
 
+        self.first_conv = DoubleConv(num_channels, num_filters)
+
         for _ in range(num_layers):
-            print(f'Adding encoder layer with {num_channels} channels and {num_filters} filters')
-            encoders.append(DoubleConv(num_channels, num_filters, 3))
             num_channels = num_filters
             num_filters *= 2
+            print(f'Adding encoder layer with {num_channels} channels and {num_filters} filters')
+            encoders.append(DoubleConv(num_channels, num_filters, kernel=3))
 
         self.encoder_layers = nn.ModuleList(encoders)
 
         for _ in range(num_layers):
-            num_filters = num_channels
-            num_channels /= 2
             padding = 0 if num_filters > init_filters else 1
             print(f'Adding decoder layer with {num_filters} channels and {num_channels} filters')
-            decoders.append(DoubleConv(int(num_filters), int(num_channels), 3)
+            decoders.append(DoubleConv(int(num_filters), int(num_channels), kernel=3))
+            num_filters = num_channels
+            num_channels //= 2
 
         self.decoder_layers = nn.ModuleList(decoders)
 
@@ -51,17 +56,22 @@ class UNet(nn.Module):
 
     def forward(self, x):
 
+        x = self.first_conv(x)
+
         inputs = []
         for enc in self.encoder_layers:
-            x = torch.maxpool2(x)
-            x = enc(x)
             inputs.append(x)
+            x = F.max_pool2d(x, kernel_size=2)
+            x = enc(x)
 
         for dec, inp in zip(self.decoder_layers, inputs[::-1]):
-            x = torch.nn.functional.interpolate(x, scale_factor=2,
-                                                mode='bilinear', align_corners=True)
-            x = torch.concat)[dec(x), inp]
-            x = torch.relu(x)
+            x = F.interpolate(x, scale_factor=2,
+                                          mode='bilinear', align_corners=True)
+            x2 = dec(x)
+            padding = (inp.shape[2] - x2.shape[2]) // 2
+            x2 = F.pad(x2, [padding] * 4)
+            pdb.set_trace()
+            x = torch.concat([x2, inp])
 
         x = self.softmax(x)
 
