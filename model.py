854f89ac6f52d0b9710ef90b426fc8285a43e76e
Georgia Rajamanoharan <g.l.rajamanoharan@gmail.com>
2023-09-13 20:20:37 +0100

    model: Add skip connections and make encoder/decoder layers into proper layers.

--- model.py
+++ model.py
@@ -2,6 +2,23 @@ import pdb
 import torch
 from torch import nn
 
+class DoubleConv(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size=3):
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = torch.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = torch.relu(x)
+
+
 class UNet(nn.Module):
 
     def __init__(self, num_layers, init_filters):
@@ -14,7 +31,7 @@ class UNet(nn.Module):
 
         for _ in range(num_layers):
             print(f'Adding encoder layer with {num_channels} channels and {num_filters} filters')
-            encoders.append(nn.Conv2d(num_channels, num_filters, 3, stride=2))
+            encoders.append(DoubleConv(num_channels, num_filters, 3))
             num_channels = num_filters
             num_filters *= 2
 
@@ -25,8 +42,7 @@ class UNet(nn.Module):
             num_channels /= 2
             padding = 0 if num_filters > init_filters else 1
             print(f'Adding decoder layer with {num_filters} channels and {num_channels} filters')
-            decoders.append(nn.ConvTranspose2d(int(num_filters), int(num_channels), 3,
-                                                          stride=2, output_padding=padding))
+            decoders.append(DoubleConv(int(num_filters), int(num_channels), 3)
 
         self.decoder_layers = nn.ModuleList(decoders)
 
@@ -35,12 +51,16 @@ class UNet(nn.Module):
 
     def forward(self, x):
 
+        inputs = []
         for enc in self.encoder_layers:
+            x = torch.maxpool2(x)
             x = enc(x)
-            x = torch.relu(x)
+            inputs.append(x)
 
-        for dec in self.decoder_layers:
-            x = dec(x)
+        for dec, inp in zip(self.decoder_layers, inputs[::-1]):
+            x = torch.nn.functional.interpolate(x, scale_factor=2,
+                                                mode='bilinear', align_corners=True)
+            x = torch.concat)[dec(x), inp]
             x = torch.relu(x)
 
         x = self.softmax(x)
